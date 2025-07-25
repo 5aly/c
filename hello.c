567e0b8ea475ec5de64d5584de95bd3f480a8f86
5aly <anjalyunninair03@gmail.com>
2025-07-25 16:17:25 +0530

    sum

--- hello.c
+++ hello.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
-void sum(int a, int b, int c){
-    int sum= a+b+c;
+int main(){
+    int a; int b;
+    printf("\n enter any number for a");
+    scanf("%d",&a);
+      printf("\n enter any number for b");
+      scanf("%d",&b);
+    int sum =a+b;
     printf("%d",sum);
-
-}
-void printcollegedetails(){
-    printf("\n snmimt");
-}
-int sumwithreturn(int a, int b){
-    return a+b;
-}
-int main()
-{
-   sum(3,5,1);
-   printcollegedetails();
-   int result =sumwithreturn(4,8);
-   printf( "\n %d",result);
 }
